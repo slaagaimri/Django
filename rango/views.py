01c71db2ce1c9482ccb25d3cfa5b293fbfaf6fba
Ching Laam Chan <laam@Chings-MacBook-Air.local>
2024-02-05 21:59:49 +0000

    Chp3.6-4

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request, 'rango/index.html', context=context_dict)
 # Create your views here.
+
+def about(request):
+    return render(request, 'rango/about.html') 
